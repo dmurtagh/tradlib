7216a9a465f22c21e81e79c5a30560a4b420300a
Damien Murtagh <damien.murtagh@gmail.com>
2022-12-26 22:36:21 -0800

    Added a simple objective-c interface
    Testing the Swift <-> c++ bridge

---
+++ tradlib/bridging/TradlibInterface.h
@@ -0,0 +1,13 @@
+//
+//  tradlib-BridgingHeader.m
+//  Landmarks
+//
+//  Created by damien murtagh on 12/26/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TradlibInterface : NSObject
+- (void) transcribeAudio;
+@end
+
---
+++ tradlib/bridging/TradlibInterface.mm
@@ -0,0 +1,22 @@
+//
+//  tradlib-BridgingHeader.m
+//  Landmarks
+//
+//  Created by damien murtagh on 12/26/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "TradlibInterface.h"
+
+#include "ODCFTranscriber.hpp"
+
+using namespace tradlib;
+
+@implementation TradlibInterface
+- (void) transcribeAudio
+{
+    ODCFTranscriber odcfTranscriber;
+    //odcfTranscriber.setAudioData(<#const SharedByteVec &audioData#>, <#SharedFloatVec &signal#>, <#int sampleRate#>, <#const std::string &fundamentalNote#>)
+    odcfTranscriber.transcribe();
+}
+@end
